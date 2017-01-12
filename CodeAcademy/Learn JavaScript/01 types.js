8864e049e1b004e23435d7e5a4e10d92495e385c
Andres Gomez <and-gome@uniandes.edu.co>
2017-01-12 18:35:41 -0500

    First Chapter of Code Academy's Learn JavaScript

---
+++ CodeAcademy/Learn JavaScript/01 types.js
@@ -0,0 +1,9 @@
+var myString = "Andres";
+var myNumber = 23;
+var myBoolean = false;
+
+
+// Do not edit the code under this line
+console.log("Name: " + myString);
+console.log("Lucky Number: " + myNumber);
+console.log("Good joke? " + myBoolean);
---
+++ CodeAcademy/Learn JavaScript/03 mathOperators.js
@@ -0,0 +1,4 @@
+console.log(31 + 3.5);
+console.log(2017 - 1969);
+console.log(65 / 240);
+console.log(65 / 240 * 100);
